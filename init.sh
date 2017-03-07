466b79e0ab147ee02c58a6ea2a865c3752904f32
alex <4tedev@gmail.com>
2017-03-07 11:40:48 +0300

    nginx config

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 sudo rm -rf /home/etc/nginx/sites-enabled/default
-sudo rm /etc/nginx/sites-enabled/test.conf
 
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
